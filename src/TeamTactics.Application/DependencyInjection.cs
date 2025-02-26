e3ede5609fe5c7636e7ef713622b3d9f3d4e1594
NJ-DELFI <84667145+SirJul1337@users.noreply.github.com>
2025-02-26 17:55:03 +0100

    Initial commit for structure

---
+++ src/TeamTactics.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TeamTactics.Application.Services.Implementation;
+using TeamTactics.Application.Services.Interfaces;
+
+namespace TeamTactics.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IHealthCheckService, HealthCheckService>();
+        return services;
+    }
+}
---
+++ src/TeamTactics.Application/Services/Implementation/HealthCheckService.cs
@@ -0,0 +1,13 @@
+﻿
+using TeamTactics.Application.Models;
+using TeamTactics.Application.Services.Interfaces;
+
+namespace TeamTactics.Application.Services.Implementation;
+
+internal class HealthCheckService : IHealthCheckService
+{
+    public Task<HealthStatus> CheckApplicationHealth()
+    {
+        throw new NotImplementedException();
+    }
+}
