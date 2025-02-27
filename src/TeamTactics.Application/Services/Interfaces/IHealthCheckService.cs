a8d0ba99635ca03232fa645e612c6248d2128b29
Hova <88477974+HovaUseles@users.noreply.github.com>
2025-02-27 20:49:03 +0100

    Cleaned up Health checks

--- src/TeamTactics.Application/Services/Interfaces/IHealthCheckService.cs
+++
@@ -1,8 +0,0 @@
-﻿using TeamTactics.Application.Models;
-
-namespace TeamTactics.Application.Services.Interfaces;
-
-public interface IHealthCheckService
-{
-    Task<HealthStatus> CheckApplicationHealth();
-}
