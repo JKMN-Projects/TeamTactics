ac5278ad58863f20ad7507c59d1e73e0ca59cf8e
Hova <88477974+HovaUseles@users.noreply.github.com>
2025-03-03 17:06:01 +0100

    Added manager and endpoint

---
+++ src/TeamTactics.Application/Teams/ITeamRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using TeamTactics.Application.Common.Interfaces;
+using TeamTactics.Domain.Teams;
+
+namespace TeamTactics.Application.Teams
+{
+    public interface ITeamRepository : ICrudRepository<Team, int>
+    {
+        public Task<IEnumerable<Team>> FindUserTeamsAsync(int userId);
+    }
+}
