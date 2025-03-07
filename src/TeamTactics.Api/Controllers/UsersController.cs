3fde574b96c474f456f2c97d29c84b57ac1e94d0
HovaUseless <bocaj.nielsen@hotmail.com>
2025-03-07 11:50:14 +0100

    Made safer create user error response

--- src/TeamTactics.Api/Controllers/UsersController.cs
+++ src/TeamTactics.Api/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 using TeamTactics.Api.Requests.Users;
 using TeamTactics.Application.Users;
 
@@ -9,18 +10,39 @@ namespace TeamTactics.Api.Controllers
     public class UsersController : ControllerBase
     {
         private readonly UserManager _userManager;
+        private readonly ILogger<UsersController> _logger;
 
-        public UsersController(UserManager userManager)
+        public UsersController(UserManager userManager, ILogger<UsersController> logger)
         {
             _userManager = userManager;
+            _logger = logger;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> CreateUser([FromBody] RegisterUserRequest request)
         {
-            await _userManager.CreateUserAsync(request.Username, request.Email, request.Password);
-            return Ok();
+            var safeResult = Problem(
+                statusCode: StatusCodes.Status400BadRequest,
+                title: "Username, email or password was incorrect."
+                );
+
+            try
+            {
+                await _userManager.CreateUserAsync(request.Username, request.Email, request.Password);
+                return Ok();
+            }
+            catch (ArgumentException ex)
+            {
+                _logger.LogError(ex, "Failed to create user");
+                return safeResult;
+            }
+            catch (PostgresException ex)
+            {
+                _logger.LogError(ex, "Failed to create user");
+                return safeResult;
+            }
         }
+
         [HttpGet("{userId}/Profile")]
         public async Task<IActionResult> GetProfile(int userId)
         {
