661b7d5edf9deb7843998e870de14f9bb9718ba3
Hova <88477974+HovaUseles@users.noreply.github.com>
2025-03-24 09:52:14 +0100

    Added get match points

--- src/TeamTactics.Application/Points/PointResultDto.cs
+++ src/TeamTactics.Application/Points/PointResultDto.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace TeamTactics.Application.Points;
 
-public sealed record PointResultDto(string playerName, string clubName, string pointCategoryName, int occurrences, decimal totalPoints);
+public sealed record PointResultDto(
+    string PlayerName,
+    string ClubName,
+    string PointCategoryName,
+    int Occurrences,
+    decimal TotalPoints);
